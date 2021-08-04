3169535e7dc0144f86cf6ca834a39c4a175fd6a4
Ruggery21 <randy.flores.22@my.csun.edu>
2021-08-03 20:32:40 -0700

    Initialization Button of new database complete.

--- app/src/main/java/com/ruggery/databasedesign/HomeActivity.java
+++ app/src/main/java/com/ruggery/databasedesign/HomeActivity.java
@@ -3,12 +3,33 @@ package com.ruggery.databasedesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button initializeButton;
+
+    MultiDBHelper multiDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initializeButton = findViewById(R.id.initializeButton);
+
+        //Database this here
+        multiDB = new MultiDBHelper(this);
+
+        initializeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Boolean initial = multiDB.initialize("null");
+                if(initial == false){
+                    Toast.makeText(HomeActivity.this, "Initializing Successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
