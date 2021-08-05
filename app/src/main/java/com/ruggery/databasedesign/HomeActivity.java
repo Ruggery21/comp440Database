eedd378d466c943dc296db4a78790e99ac8cf80e
My Tran <my.tran.811@my.csun.edu>
2021-08-05 15:46:23 -0700

    added new functions: display sqlite data in recycler view

--- app/src/main/java/com/ruggery/databasedesign/HomeActivity.java
+++ app/src/main/java/com/ruggery/databasedesign/HomeActivity.java
@@ -1,14 +1,143 @@
 package com.ruggery.databasedesign;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    /*
+    ArrayList<String> subjectNames;
+    MyRecyclerViewAdapter adapter; */
+
+    ArrayList<NoteModel> arrayList;
+    RecyclerView recyclerView;
+    FloatingActionButton actionButton;
+    DatabaseHelper database_helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        /*
+        // data to populate the RecyclerView with
+        subjectNames = new ArrayList<String>();
+        for(int i = 1; i < 8; i++) {
+            subjectNames.add("Subject: " + i + "\nDescription: " + i + "\nTags: " + i);
+        } */
+
+        /*
+        animalNames.add("Computer Science Personal Statement");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat"); */
+
+        // set up the RecyclerView
+        /*
+        RecyclerView recyclerView = findViewById(R.id.rvSubject);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, subjectNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration); */
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        actionButton = (FloatingActionButton) findViewById(R.id.add);
+        database_helper = new DatabaseHelper(this);
+        displayNotes();
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
     }
+
+    //display notes list
+    public void displayNotes() {
+        arrayList = new ArrayList<>(database_helper.getNotes());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        NotesAdapter adapter = new NotesAdapter(getApplicationContext(), this, arrayList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    //display dialog
+    public void showDialog() {
+        final EditText title, des;
+        Button submit;
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        dialog.setContentView(R.layout.dialog);
+        params.copyFrom(dialog.getWindow().getAttributes());
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.gravity = Gravity.CENTER;
+        dialog.getWindow().setAttributes(params);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        title = (EditText) dialog.findViewById(R.id.title);
+        des = (EditText) dialog.findViewById(R.id.description);
+        submit = (Button) dialog.findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {;
+            @Override
+            public void onClick(View v) {
+                if (title.getText().toString().isEmpty()) {
+                    title.setError("Please Enter Title");
+                }else if(des.getText().toString().isEmpty()) {
+                    des.setError("Please Enter Description");
+                }else {
+                    database_helper.addNotes(title.getText().toString(), des.getText().toString());
+                    dialog.cancel();
+                    displayNotes();
+                }
+            }
+        });
+    }
+
+    /*
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButtonClick(View view) {
+        insertSingleItem();
+    }
+
+    private void insertSingleItem() {
+        String item = "Pig";
+        int insertIndex = 2;
+        subjectNames.add(insertIndex, item);
+        adapter.notifyItemInserted(insertIndex);
+    } */
+
+    //new code
+
+
 }
