ef67a20942c4c9749a1b86e1c312094f5689a8c7
Emanuel-9810 <60586959+Emanuel-9810@users.noreply.github.com>
2021-08-23 13:56:27 -0700

    Update to followers

--- app/src/main/java/com/ruggery/databasedesign/FollowActivity.java
+++ app/src/main/java/com/ruggery/databasedesign/FollowActivity.java
@@ -6,12 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FollowActivity extends AppCompatActivity {
 
-    private TextView mTextView;
     MultiDBHelper multiDBHelper;
     private EditText searchUser;
     private Button followBtn;
@@ -22,18 +22,29 @@ public class FollowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_follow);
 
-        mTextView = (TextView) findViewById(R.id.text);
+//        mTextView = (TextView) findViewById(R.id.text);
 
         multiDBHelper = new MultiDBHelper(this);
+        initiate();
+
+        String user = getIntent().getStringExtra("user_key");
 
         followBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = searchUser.getText().toString();
-                String result = multiDBHelper.posBlog(user);
+                String leader = searchUser.getText().toString();
+                String result = multiDBHelper.posBlog(leader);
                 userFound.setText(result);
+
+                if(result != null){
+                    multiDBHelper.addFollower(leader, user);
+                    Toast.makeText(FollowActivity.this, "You have now followed " + leader, Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
 
+    private void initiate() {
+        userFound = findViewById(R.id.userFound);
     }
 }
